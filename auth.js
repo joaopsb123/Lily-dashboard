211cdaf564a9e9e5bfa0c4050049e9392cda3a47
joaopsb123 <joaopsb12223@gmail.com>
2025-07-12 18:01:43 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 import { cookies } from 'next/headers';
 
 export async function GET() {
-  const authUrl = new URL('https://discord.com/oauth2/authorize?client_id=1388158298305597483&response_type=code&redirect_uri=https%3A%2F%2Flily-red.vercel.app%2F%3Fcode%3DwJoTdraGnLt7C88PL0oMWRCnd8R7dr%2Fapi%2Fcallback&scope=identify+email+guilds');
+  const authUrl = new URL('https://discord.com/oauth2/authorize?client_id=1388158298305597483&response_type=code&redirect_uri=https%3A%2F%2Flily-dashboard2.vercel.app%2Fapi%2Fcallback&scope=identify+email+guilds');
   authUrl.searchParams.set('client_id', process.env.DISCORD_CLIENT_ID);
   authUrl.searchParams.set('redirect_uri', `${process.env.VERCEL_URL}/api/callback`);
   authUrl.searchParams.set('response_type', 'code');
