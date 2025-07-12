161731fa03ea7da1255ba44842c8a5235838f259
joaopsb123 <joaopsb12223@gmail.com>
2025-07-12 18:21:39 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,17 +1,11 @@
-// /api/auth
-import { cookies } from 'next/headers';
+// api/auth.js
+export default async (req, res) => {
+  const redirectURL = new URL('https://discord.com/oauth2/authorize');
+  redirectURL.searchParams.set('client_id', '1388158298305597483');
+  redirectURL.searchParams.set('redirect_uri', 'https://lily-dashboard2.vercel.app/dashboard.html');
+  redirectURL.searchParams.set('response_type', 'code');
+  redirectURL.searchParams.set('scope', 'identify email');
 
-export async function GET() {
-  const authUrl = new URL('https://discord.com/oauth2/authorize?client_id=1388158298305597483&response_type=code&redirect_uri=https%3A%2F%2Flily-dashboard2.vercel.app&scope=identify+email+guilds');
-  authUrl.searchParams.set('client_id', process.env.DISCORD_CLIENT_ID);
-  authUrl.searchParams.set('redirect_uri', `${process.env.VERCEL_URL}/api/callback`);
-  authUrl.searchParams.set('response_type', 'code');
-  authUrl.searchParams.set('scope', 'identify email');
-
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: authUrl.toString(),
-    },
-  });
-}
+  res.writeHead(302, { Location: redirectURL.toString() });
+  res.end();
+};
