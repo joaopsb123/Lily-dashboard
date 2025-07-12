58d87859cb65c3df39c0b4acd243ac524f37667f
joaopsb123 <joaopsb12223@gmail.com>
2025-07-12 18:22:26 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // api/auth.js
 export default async (req, res) => {
-  const redirectURL = new URL('https://discord.com/oauth2/authorize');
+  const redirectURL = new URL('https://discord.com/oauth2/authorize?client_id=1388158298305597483&response_type=code&redirect_uri=https%3A%2F%2Flily-dashboard2.vercel.app%2F&scope=identify+email+guilds');
   redirectURL.searchParams.set('client_id', '1388158298305597483');
   redirectURL.searchParams.set('redirect_uri', 'https://lily-dashboard2.vercel.app/dashboard.html');
   redirectURL.searchParams.set('response_type', 'code');
