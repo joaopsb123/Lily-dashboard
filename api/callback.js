7f2e4755b1816b8862869c139d3a3dfae7a2b3aa
joaopsb123 <joaopsb12223@gmail.com>
2025-07-13 17:26:53 +0100

    Add files via upload

---
+++ api/callback.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) return res.status(400).json({ error: 'Faltando o code' });
+
+  const params = new URLSearchParams();
+  params.append('client_id', '1392176069070557365');
+  params.append('client_secret', '0DNQT5RRwFYPtJ2rk1bQqqElmsIoHdM6');
+  params.append('grant_type', 'authorization_code');
+  params.append('code', code);
+  params.append('redirect_uri', 'https://SEUSITE.vercel.app/callback');
+  params.append('scope', 'identify guilds');
+
+  const tokenRes = await fetch('https://discord.com/api/oauth2/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: params
+  });
+
+  const tokenData = await tokenRes.json();
+
+  const userRes = await fetch('https://discord.com/api/users/@me', {
+    headers: { Authorization: `Bearer ${tokenData.access_token}` }
+  });
+
+  const userData = await userRes.json();
+
+  res.status(200).json(userData);
+}
